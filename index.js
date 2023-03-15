7f1d568d197ef76ffadbdcc3646ace619528f390
Viktor Kursin <viktorkursin1@gmail.com>
2023-03-15 16:43:41 +0400

    новый функционал: комментарии

--- index.js
+++ index.js
@@ -71,3 +71,82 @@ const addRepository = (repository) => {
 };
 
 input.addEventListener('input', searchRepositories);
+
+const commentsList = document.getElementById('comments-list');
+
+function formatTime(time) {
+  const today = new Date();
+  const date = new Date(time);
+  const diffTime = Math.abs(today - date);
+  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+  if (diffDays === 0) {
+    return 'сегодня ' + date.toLocaleTimeString();
+  } else if (diffDays === 1) {
+    return 'вчера ' + date.toLocaleTimeString();
+  } else {
+    return date.toLocaleString();
+  }
+}
+
+function addComment(name, comment, time, id) {
+  const commentDiv = document.createElement('div');
+  commentDiv.classList.add('comment');
+  commentDiv.dataset.id = id;
+
+  const nameSpan = document.createElement('span');
+  const timeSpan = document.createElement('span');
+  const commentP = document.createElement('p');
+
+  nameSpan.textContent = name + ': ';
+  timeSpan.textContent = formatTime(time);
+  commentP.textContent = comment;
+
+  commentDiv.appendChild(nameSpan);
+  commentDiv.appendChild(timeSpan);
+  commentDiv.appendChild(commentP);
+  commentsList.appendChild(commentDiv);
+
+  const commentActionsDiv = document.createElement('div');
+  commentActionsDiv.classList.add('comment-actions');
+
+  const likeButton = document.createElement('button');
+  likeButton.textContent = '❤️';
+  likeButton.addEventListener('click', () => {
+    likeComment(id);
+  });
+  commentActionsDiv.appendChild(likeButton);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Удалить';
+  deleteButton.addEventListener('click', () => {
+    deleteComment(id);
+  });
+  commentActionsDiv.appendChild(deleteButton);
+
+  commentDiv.appendChild(commentActionsDiv);
+}
+function likeComment(id) {
+  const commentDiv = document.querySelector(`.comment[data-id="${id}"]`);
+  const likeButton = commentDiv.querySelector('button:first-of-type');
+  const likesSpan = document.createElement('span');
+  likesSpan.textContent = '1';
+  likesSpan.classList.add('likes');
+  likeButton.replaceWith(likesSpan);
+}
+
+function deleteComment(id) {
+  const commentDiv = document.querySelector(`.comment[data-id="${id}"]`);
+  commentDiv.remove();
+}
+
+const commentForm = document.querySelector('#comment-form form');
+let commentId = 0;
+
+commentForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const comment = document.getElementById('comment').value;
+  const name = document.getElementById('name').value;
+  const time = new Date().toISOString();
+  addComment(name, comment, time, commentId++);
+  commentForm.reset();
+});
