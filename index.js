688ed24e68e6a249793662b9a24d9823dad701c5
Viktor Kursin <viktorkursin1@gmail.com>
2023-03-16 17:16:08 +0400

    добавление выбора даты

--- index.js
+++ index.js
@@ -1,77 +1,3 @@
-const input = document.querySelector('input');
-const autocomplete = document.querySelector('.autocomplete');
-const repositoriesList = document.querySelector('.repositories');
-
-const debounce = (func, delay) => {
-  let timeoutId;
-  return (...args) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay);
-  };
-};
-
-const getRepositories = async (query) => {
-  const response = await fetch(`https://api.github.com/search/repositories?q=${query}&per_page=5`);
-  const data = await response.json();
-  return data.items;
-};
-
-const clearAutocomplete = () => {
-  while (autocomplete.firstChild) {
-    autocomplete.removeChild(autocomplete.firstChild);
-  }
-};
-
-const renderAutocomplete = (repositories) => {
-  clearAutocomplete();
-  repositories.forEach((repository) => {
-    const li = document.createElement('li');
-    li.textContent = repository.full_name;
-    li.addEventListener('click', () => {
-      addRepository(repository);
-      clearAutocomplete();
-      input.value = '';
-    });
-    autocomplete.appendChild(li);
-  });
-};
-
-const searchRepositories = debounce(async () => {
-  const query = input.value.trim();
-  if (query) {
-    const repositories = await getRepositories(query);
-    renderAutocomplete(repositories);
-  } else {
-    clearAutocomplete();
-  }
-}, 300);
-
-const addRepository = (repository) => {
-  const li = document.createElement('li');
-  const title = document.createElement('h2');
-  title.textContent = repository.name;
-  const owner = document.createElement('p');
-  owner.textContent = `Owner: ${repository.owner.login}`;
-  const stars = document.createElement('p');
-  stars.textContent = `Stars: ${repository.stargazers_count}`;
-  const button = document.createElement('button');
-  button.textContent = 'Remove';
-  button.addEventListener('click', () => {
-    li.remove();
-  });
-  li.appendChild(title);
-  li.appendChild(owner);
-  li.appendChild(stars);
-  li.appendChild(button);
-  repositoriesList.appendChild(li);
-};
-
-input.addEventListener('input', searchRepositories);
-
 const commentsList = document.getElementById('comments-list');
 
 function formatTime(time) {
@@ -88,21 +14,21 @@ function formatTime(time) {
   }
 }
 
-function addComment(name, comment, time, id) {
+function addComment(name, comment, date, id) {
   const commentDiv = document.createElement('div');
   commentDiv.classList.add('comment');
   commentDiv.dataset.id = id;
 
   const nameSpan = document.createElement('span');
-  const timeSpan = document.createElement('span');
+  const dateSpan = document.createElement('span');
   const commentP = document.createElement('p');
 
   nameSpan.textContent = name + ': ';
-  timeSpan.textContent = formatTime(time);
+  dateSpan.textContent = formatTime(date);
   commentP.textContent = comment;
 
   commentDiv.appendChild(nameSpan);
-  commentDiv.appendChild(timeSpan);
+  commentDiv.appendChild(dateSpan);
   commentDiv.appendChild(commentP);
   commentsList.appendChild(commentDiv);
 
@@ -125,6 +51,7 @@ function addComment(name, comment, time, id) {
 
   commentDiv.appendChild(commentActionsDiv);
 }
+
 function likeComment(id) {
   const commentDiv = document.querySelector(`.comment[data-id="${id}"]`);
   const likeButton = commentDiv.querySelector('button:first-of-type');
@@ -146,7 +73,8 @@ commentForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const comment = document.getElementById('comment').value;
   const name = document.getElementById('name').value;
-  const time = new Date().toISOString();
-  addComment(name, comment, time, commentId++);
+  const dateInput = document.getElementById('date');
+  const dateValue = dateInput.value ? new Date(dateInput.value).toISOString() : new Date().toISOString();
+  addComment(name, comment, dateValue, commentId++);
   commentForm.reset();
 });
